1010d109e10bc116506b1f261c09a6f3e462e7d5
AlexRashidov <rashidov.sasha@yandex.ru>
2024-02-16 10:44:52 +0700

    HeroController script v1
    not work

--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private Rigidbody2D rb; //ссылка на компонент Rigidbody2d
+    public float speed = 1.0f;
+    private Vector2 moveVector;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveVector.x = Input.GetAxis("Horizontal");
+        moveVector.y = Input.GetAxis("Vertical");
+        rb.MovePosition(rb.position + moveVector * speed * Time.deltaTime);
     }
 }
